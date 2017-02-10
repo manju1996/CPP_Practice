db41c45a7eaeda24b75ae46cda438395783ac572
manju1996 <malk14ec@cmrit.ac.in>
2017-02-10 10:01:53 +0530

    Delete complex.c

--- complex.c
+++
@@ -1,8 +0,0 @@
-#include"complex.h"
-complex add(complex a,complex b)
- {
-   complex c;
-   c.real=a.real-b.real;
-    c.img=a.img-b.img;
-    return(c);                                                 
-  }
